20116ec97017f7480cae5863ca89c069feb711f9
Agung Kusaeri <agung.kusaeri9@gmail.com>
2024-11-25 12:52:38 +0700

    fix: fix access menu by role

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -16,49 +16,61 @@
                 </div>
             </div>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}">
-                <i class="typcn typcn-device-desktop menu-icon"></i>
-                <span class="menu-title">Dashboard </span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="typcn typcn-briefcase menu-icon"></i>
-                <span class="menu-title">Manajemen User</span>
-                <i class="typcn typcn-chevron-right menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('roles.index') }}">Role</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('permissions.index') }}">Permission</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('users.index') }}">User</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#master-ui" aria-expanded="false"
-                aria-controls="master-ui">
-                <i class="typcn typcn-briefcase menu-icon"></i>
-                <span class="menu-title">Master Data</span>
-                <i class="typcn typcn-chevron-right menu-arrow"></i>
-            </a>
-            <div class="collapse" id="master-ui">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('departments.index') }}">Department</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('units.index') }}">Unit</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
+        @can('Dashboard')
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('dashboard') }}">
+                    <i class="typcn typcn-device-desktop menu-icon"></i>
+                    <span class="menu-title">Dashboard </span>
+                </a>
+            </li>
+        @endcan
+        @canany(['Role Index', 'Permission Index', 'User Index'])
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                    <i class="typcn typcn-briefcase menu-icon"></i>
+                    <span class="menu-title">Manajemen User</span>
+                    <i class="typcn typcn-chevron-right menu-arrow"></i>
+                </a>
+                <div class="collapse" id="ui-basic">
+                    <ul class="nav flex-column sub-menu">
+                        @can('Role Index')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('roles.index') }}">Role</a>
+                            </li>
+                        @endcan
+                        @can('Permission Index')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('permissions.index') }}">Permission</a>
+                            </li>
+                        @endcan
+                        @can('User Index')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('users.index') }}">User</a>
+                            </li>
+                        @endcan
+                    </ul>
+                </div>
+            </li>
+        @endcanany
+        @canany(['Department Index', 'Unit Index'])
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#master-ui" aria-expanded="false"
+                    aria-controls="master-ui">
+                    <i class="typcn typcn-briefcase menu-icon"></i>
+                    <span class="menu-title">Master Data</span>
+                    <i class="typcn typcn-chevron-right menu-arrow"></i>
+                </a>
+                <div class="collapse" id="master-ui">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('departments.index') }}">Department</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('units.index') }}">Unit</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        @endcanany
     </ul>
 </nav>
