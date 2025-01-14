d9207171f2ebfc729cb2b6cbf4728d67a62f5d01
Agung Kusaeri <agung.kusaeri9@gmail.com>
2025-01-14 08:49:01 +0700

    fix: avatar in users

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
         <li class="nav-item">
             <div class="d-flex sidebar-profile">
                 <div class="sidebar-profile-image">
-                    <img src="{{ asset('assets') }}/images/faces/face29.png" alt="image">
+                    <img src="{{ auth()->user()->avatar() }}" alt="image">
                     <span class="sidebar-status-indicator"></span>
                 </div>
                 <div class="sidebar-profile-name">
